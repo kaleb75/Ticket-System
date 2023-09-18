02f4e727f3be88190344144f360e60b9549a0870
kaleb75 <44348427+kaleb75@users.noreply.github.com>
2023-09-18 15:36:04 -0600

    Update TestDocumentacion.java

--- TicketSystem/src/Tickets/TestDocumentacion.java
+++ TicketSystem/src/Tickets/TestDocumentacion.java
@@ -4,6 +4,14 @@
  */
 package Tickets;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
+
 /**
  *
  * @author IMX078856
@@ -30,12 +38,23 @@ public class TestDocumentacion extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane4 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        Docu = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
         jInternalFrame2 = new javax.swing.JInternalFrame();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Documentation");
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         jInternalFrame1.setClosable(true);
         jInternalFrame1.setIconifiable(true);
@@ -48,9 +67,20 @@ public class TestDocumentacion extends javax.swing.JFrame {
         jLabel1.setText("Documentation:");
         jScrollPane1.setViewportView(jLabel1);
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane4.setViewportView(jTextArea1);
+        Docu.setColumns(20);
+        Docu.setRows(5);
+        jScrollPane4.setViewportView(Docu);
+
+        jButton1.setText("Enviar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Attached 1:");
+
+        jButton2.setText("Search");
 
         javax.swing.GroupLayout jInternalFrame1Layout = new javax.swing.GroupLayout(jInternalFrame1.getContentPane());
         jInternalFrame1.getContentPane().setLayout(jInternalFrame1Layout);
@@ -59,15 +89,29 @@ public class TestDocumentacion extends javax.swing.JFrame {
             .addGroup(jInternalFrame1Layout.createSequentialGroup()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
-            .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 405, Short.MAX_VALUE)
+            .addComponent(jScrollPane4)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jInternalFrame1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jInternalFrame1Layout.setVerticalGroup(
             jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jInternalFrame1Layout.createSequentialGroup()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 296, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 188, Short.MAX_VALUE))
+                .addComponent(jScrollPane4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel2)
+                        .addComponent(jButton2))
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
         );
 
         jInternalFrame2.setClosable(true);
@@ -79,15 +123,32 @@ public class TestDocumentacion extends javax.swing.JFrame {
 
         jScrollPane2.setViewportView(jTextPane1);
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "ticketID", "title", "priority", "status", "assigned", "ets", "description", "documentacion"
+            }
+        ));
+        jScrollPane3.setViewportView(jTable1);
+
         javax.swing.GroupLayout jInternalFrame2Layout = new javax.swing.GroupLayout(jInternalFrame2.getContentPane());
         jInternalFrame2.getContentPane().setLayout(jInternalFrame2Layout);
         jInternalFrame2Layout.setHorizontalGroup(
             jInternalFrame2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 854, Short.MAX_VALUE)
+            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 1041, Short.MAX_VALUE)
+            .addComponent(jScrollPane2)
         );
         jInternalFrame2Layout.setVerticalGroup(
             jInternalFrame2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jInternalFrame2Layout.createSequentialGroup()
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 574, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -102,11 +163,108 @@ public class TestDocumentacion extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jInternalFrame1)
-            .addComponent(jInternalFrame2)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jInternalFrame2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       /* String ruta = "C:\\Users\\imx078856\\Documents\\GitHub\\Ticket-System\\BD\\BDTickets-System.accdb";
+    String url = "jdbc:ucanaccess://" + ruta;
+    String Documento = Docu.getText();
+    int IDTicket = 1; // Reemplaza 1 con el ID del ticket que deseas actualizar
+
+    try {
+        Connection connection = DriverManager.getConnection(url);
+        String query = "UPDATE Test SET Documentacion = ? WHERE IDTicket = ?";
+        
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, Documento);
+            preparedStatement.setInt(2, );
+            
+            int filasAfectadas = preparedStatement.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                System.out.println("Documento actualizado con éxito.");
+            } else {
+                System.out.println("No se pudo actualizar el documento.");
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    
+*/
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+ //solicitarNumeroTicket();
+ buscarDatos();
+    }//GEN-LAST:event_formWindowOpened
+/*private void solicitarNumeroTicket() {
+
+     String numeroTicketStr = JOptionPane.showInputDialog(this, "Ingrese el número de ticket:", "Ingresar Número de Ticket", JOptionPane.QUESTION_MESSAGE);
+    
+    try {
+      int NumerodeTicket = Integer.parseInt(numeroTicketStr);
+      String mensaje = "<html>Your Ticket is: <H1><b>FIS-"+NumerodeTicket+"</b></h1></html>";
+            JOptionPane.showMessageDialog(null, mensaje);
+
+    } catch (NumberFormatException e) {
+        // Si no se ingresa un número válido, puedes manejarlo aquí
+        JOptionPane.showMessageDialog(this, "Número de ticket no válido. Por favor, ingrese un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
+        solicitarNumeroTicket(); // Vuelve a solicitar el número de ticket
+    }}*/
+
+
+private void buscarDatos() {
+        String ruta = "C:\\Users\\imx078856\\Documents\\GitHub\\Ticket-System\\BD\\BDTickets-System.accdb";
+        String url = "jdbc:ucanaccess://" + ruta;
+     String numeroTicketStr = JOptionPane.showInputDialog(this, "Ingrese el número de ticket:", "Ingresar Número de Ticket", JOptionPane.QUESTION_MESSAGE);
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+             int NumerodeTicket = Integer.parseInt(numeroTicketStr);
+      String mensaje = "<html>Your Ticket is: <H1><b>FIS-"+NumerodeTicket+"</b></h1></html>";
+            JOptionPane.showMessageDialog(null, mensaje);
+
+        String query = "SELECT TicketID, Title, Priority, Status, Assigned, ETS, Descripcion, Documentacion FROM TuTabla WHERE TicketID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, NumerodeTicket);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+         if (resultSet.next()) {
+            // Aquí puedes obtener los valores de las columnas correspondientes
+            int ticketID = resultSet.getInt("TicketID");
+            String title = resultSet.getString("Title");
+            String priority = resultSet.getString("Priority");
+            String status = resultSet.getString("Status");
+            String assigned = resultSet.getString("Assigned");
+            String ets = resultSet.getString("ETS");
+            String descripcion = resultSet.getString("Descripcion");
+            String documentacion = resultSet.getString("Documentacion");
+
+            // Haz algo con los valores obtenidos, por ejemplo, mostrarlos en tu formulario
+            campoTicketID.setText(String.valueOf(ticketID));
+            campoTitle.setText(title);
+            campoPriority.setText(priority);
+            campoStatus.setText(status);
+            campoAssigned.setText(assigned);
+            campoETS.setText(ets);
+            campoDescripcion.setText(descripcion);
+            campoDocumentacion.setText(documentacion);
+        } else {
+            // Si no se encuentra un registro con ese TicketID, puedes manejarlo aquí
+            JOptionPane.showMessageDialog(this, "No se encontraron datos para el TicketID proporcionado.", "Información", JOptionPane.INFORMATION_MESSAGE);
+        }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de errores, puedes personalizarlo según tus necesidades
+        }
+
+}
+
 
     /**
      * @param args the command line arguments
@@ -144,13 +302,18 @@ public class TestDocumentacion extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea Docu;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JInternalFrame jInternalFrame1;
     private javax.swing.JInternalFrame jInternalFrame2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
 }
